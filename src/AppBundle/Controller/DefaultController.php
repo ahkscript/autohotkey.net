0c92f26d8df3c924a6c512d34787c0203bbb42a1
Charles Simmons <ttnnkkrr@gmail.com>
2017-08-30 18:39:02 -0400

    added directory browser

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,9 +14,40 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $baseDir = realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR."web";
+        $finder = new Finder;
+        $iterator = $finder
+            ->directories()
+            ->name('~*')
+            ->depth(0)
+            ->in($baseDir);
+        //print_r($iterator);
+        $i=0;
+        foreach(iterator_to_array($iterator) as $path)
+            $iterator1[$i++] = $this->generateUrl('homepage').str_replace($baseDir.DIRECTORY_SEPARATOR,'',$path);
+        //print_r($iterator);
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('default/default.html.twig', [
+            'base_dir' => $baseDir,'iterator' => $iterator1, //$this->generateUrl('homepage')
+        ]);
+    }
+    /**
+     * @Route("/{user}", name="UserContent")
+     */
+    public function showUserDir(Request $request, $user){
+
+        $baseDir = realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR."web".DIRECTORY_SEPARATOR
+                   .$user;
+        $finder = new Finder;
+        $iterator = $finder
+            ->files()
+            ->name('*.ahk')
+            ->in($baseDir);
+        $i=0;
+        foreach(iterator_to_array($iterator) as $path)
+            $iterator1[$i++] = $this->generateUrl('homepage').str_replace($baseDir.DIRECTORY_SEPARATOR,'',$path);
+        return $this->render('default/user.html.twig', [
+            'user' => $user,'iterator' => $iterator1, //$this->generateUrl('homepage')
         ]);
     }
 }
